46871415b89c285d34a57f3109a40b9bb9ccd605
Nick Maiorana <nmaiorana@gmail.com>
2017-10-01 18:15:43 -0400

    Initial Load

---
+++ gen/com/svi/hiita/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.svi.hiita;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/svi/hiita/HIITTreadmillAssistantApplication.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2010 Silent Viper Investments
+ */
+
+package com.svi.hiita;
+
+import android.app.Application;
+
+public class HIITTreadmillAssistantApplication extends Application {
+	@Override
+	public void onCreate() {
+	}
+
+}
